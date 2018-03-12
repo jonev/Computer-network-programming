72073a96565047ecb7665aae7528bf22f6fafe84
Martin Wangen <wangen93@hotmail.com>
2018-03-12 17:07:05 +0100

    UDP client and server with calculator

---
+++ TCPSocket/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+​using System.Net.Sockets;
+
+namespace TCPSocket
+{
+    class Server
+    {
+        public static void Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+            server.Start();
+            Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection...", Environment.NewLine);
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected.");
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    public struct Received
+    {
+        public IPEndPoint Sender;
+        public string Message;
+    }
+
+    abstract class UdpBase
+    {
+        protected UdpClient Client;
+
+        protected UdpBase()
+        {
+            Client = new UdpClient();
+        }
+
+        public async Task<Received> Receive()
+        {
+            var result = await Client.ReceiveAsync();
+            return new Received()
+            {
+                Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                Sender = result.RemoteEndPoint
+            };
+        }
+    }
+
+    //Server
+    class UdpListener : UdpBase
+    {
+        private IPEndPoint _listenOn;
+
+        public UdpListener() : this(new IPEndPoint(IPAddress.Any, 32123))
+        {
+        }
+
+        public UdpListener(IPEndPoint endpoint)
+        {
+            _listenOn = endpoint;
+            Client = new UdpClient(_listenOn);
+        }
+
+        public void Reply(string message, IPEndPoint endpoint)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length, endpoint);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new server
+            try
+            {
+                Console.WriteLine("Starting main...");
+                var server = new UdpListener();
+
+                //start listening for messages and copy the messages back to the client
+                Task.Factory.StartNew(async () => {
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting for client...");
+                        var received = await server.Receive();
+                        string result = received.Message;
+                        Console.WriteLine($"Recieved: {result}");
+                        try
+                        {
+                            string[] s = received.Message.Split(' ');
+                            int a = Int32.Parse(s[0]);
+                            int b = Int32.Parse(s[2]);
+                            int c;
+                            switch (s[1])
+                            {
+                                case "-":
+                                    c = a - b;
+                                    break;
+                                case "+":
+                                    c = a + b;
+                                    break;
+                                default:
+                                    c = 0;
+                                    break;
+                            }
+                            server.Reply("Answer from server: " + result + " = " + c.ToString(),received.Sender);
+                            Console.WriteLine($"Replying: {result} = {c.ToString()}");
+
+                        }
+                        catch (Exception ex)
+                        {
+                            server.Reply("Answer from server: Something failed. Did you use the right syntax?",received.Sender);
+                            Console.WriteLine("Somthing failed");
+                            Console.WriteLine(ex);
+                        }
+                    }
+                });
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
