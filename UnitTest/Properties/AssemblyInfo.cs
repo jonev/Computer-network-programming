fa56c44de0e925668b334d533115458b486a2657
jonev <jone.vassbo@gmail.com>
2018-03-24 09:36:08 +0100

    Unittest fixed.

--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -10,9 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90a9efa6-1d30-46ac-9375-fa59fd18099b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
